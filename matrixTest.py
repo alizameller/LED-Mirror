c32242662d711d6aa1474810967cb7d0453a216c
alizameller <alizameller@gmail.com>
2022-03-10 15:07:55 -0500

    LEDs display, orientation needs to be fixed

--- matrixTest.py
+++ matrixTest.py
@@ -4,35 +4,24 @@ import neopixel
 
 pixels = neopixel.Neopixel(board.D18, 121, brightness = 0.2, auto_write=False, pixel_order=RGBW)
 
-with open("R.txt", "r") as Rfile:
-with open("G.txt", "r") as Gfile:
-with open("B.txt", "r") as Bfile:
-
-i = 0
-j = 0
-k = 0
-
-#intilize all arrays R,G,B with size 132 to zero 
-red=[]
-red = [0 for i in range(132)] 
-green=[]
-green = [0 for i in range(132)] 
-blue=[]
-blue = [0 for i in range(132)] 
-# will varibles R,G,B be considered an int or float 
-for line in Rfile:
-        R = {}.format(line.strip())
-        #convert to int for simplicity R,G,B
-        red[i] = int(R)
-        i = i + 1
-for line in Gfile:
-        G = {}.format(line.strip())
-        green[j] = int(G)
-        j = j + 1
-for line in Bfile:
-        B = {}.format(line.strip())
-        blue[k] =  int(B)
-        k = k + 1
-# test code to print out what a value of the neopixels would be
-for a in range(132):
-        print("neoPixel[",a,"] = (",red[a],",",green[a], ",",blue[a],")")
+with open("R.txt", "r") as Rfile, open("G.txt", "r") as Gfile, open("B.txt", "r") as Bfile:
+	i = 0
+	j = 0
+	k = 0
+	l = 0
+	R = open("R.txt").read().split('\n')
+	for i in range(0, len(R)-1):
+		R[i] = int(float(R[i]))
+	G = open("G.txt").read().split('\n')
+	for j in range(0, len(G)-1):
+		G[i] = int(float(G[i]))
+	B = open("B.txt").read().split('\n')
+	for k in range(0, len(B)-1):
+		B[i] = int(float(B[i]))
+	for l in range(132):
+		print(R[l], G[l], B[l])
+		r = R[l]
+		g = G[l]
+		b = B[l]
+		pixels[l] = (r, g, b)
+	pixels.show() 
