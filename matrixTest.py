3a972574f0a3f0ca064ea8c1b4c2f2ea677ac229
alizameller <alizameller@gmail.com>
2022-04-07 12:39:29 -0400

    Updated matrixTest.py to account for "snaking"

--- matrixTest.py
+++ matrixTest.py
@@ -8,16 +8,39 @@ with open("R.txt", "r") as Rfile, open("G.txt", "r") as Gfile, open("B.txt", "r"
 	i = 0
 	j = 0
 	k = 0
-	l = 0
+	temp1 = [0]
+
 	R = open("R.txt").read().split('\n')
 	for i in range(0, len(R)-1):
 		R[i] = int(float(R[i]))
+
+	for l in range(5):
+		for m in range(6):
+			temp1 = R[24*l+12+m]
+			R[24*l+12+m] = R[24*l+23-m]
+			R[24*l+23-m] = temp1
+
+
 	G = open("G.txt").read().split('\n')
 	for j in range(0, len(G)-1):
 		G[i] = int(float(G[i]))
+
+	for l in range(5):
+		for m in range(6):
+			temp1 = G[24*l+12+m]
+			G[24*l+12+m] = G[24*l+23-m]
+			G[24*l+23-m] = temp1
+
 	B = open("B.txt").read().split('\n')
 	for k in range(0, len(B)-1):
 		B[i] = int(float(B[i]))
+
+	for l in range(5):
+		for m in range(6):
+			temp1 = B[24*l+12+m]
+			B[24*l+12+m] = B[24*l+23-m]
+			B[24*l+23-m] = temp1
+
 	for l in range(132):
 		print(R[l], G[l], B[l])
 		r = R[l]
